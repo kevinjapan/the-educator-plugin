d124fb0169f30821dbac8cb77f7d627ab80f88f2
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-02-08 14:31:46 +0000

    update form field UI elements

--- views/course_custom_post_metabox.php
+++ views/course_custom_post_metabox.php
@@ -3,63 +3,78 @@
 // Custom Fields
 // tagline, topics, ucas_code, duration, study_mode, start_month, learning_mode
 
-
-// future : limit input text lengths - rollout
+// to do : limit input text lengths - rollout
 
 ?>
 
-<div class="form-wrap">
-   <div class="form-field term-image-wrap">
-      <label for="te_course_custom_metabox_tagline">Tagline</label>
-      <input   value="<?php echo esc_attr($details['tagline']); ?>" type="text"
-               name="_te_course_array_fields[tagline]" id="te_course_tagline_field"  >
-   </div>
-   <div class="form-field term-image-wrap">
-      <label for="te_course_custom_metabox_topics">Topics</label>
-      <span>Enter a comma-separated list of topics for this course:</span>
-      <input   value="<?php echo esc_attr($details['topics']); ?>" type="text"
-               name="_te_course_array_fields[topics]" id="te_course_topics_field"  >
-   </div>            
-   <div class="form-field term-image-wrap">
-      <label for="te_course_custom_metabox_ucas_code">UCAS code</label>
-      <input   value="<?php echo esc_attr($details['ucas_code']); ?>" type="text"
-               name="_te_course_array_fields[ucas_code]" id="te_course_ucas_code_field"  > 
-   </div>
-   <div class="form-field term-image-wrap">
-      <label for="te_course_custom_metabox_duration">Duration</label>
-      <input   value="<?php echo esc_attr($details['duration']); ?>" type="text"
-               name="_te_course_array_fields[duration]" id="te_course_duration_field"  >
-   </div>
-   <div class="form-field term-image-wrap">
-      <label for="te_course_custom_metabox_study_mode">Study Mode</label>
+   <label class="te_label">
+      <span class="te_label_text">Tagline</span>
+      <input   
+         id="te_course_tagline_field"
+         name="_te_course_array_fields[tagline]"
+         value="<?php echo esc_attr($details['tagline']); ?>" 
+         type="text">
+   </label>
+
+   <label class="te_label">
+      <span class="te_label_text">Topics</span>
+      <!-- to do : tidy layout while including: <span>Enter a comma-separated list of topics for this course:</span> -->
+         <input    
+            id="te_course_topics_field"
+            name="_te_course_array_fields[topics]"
+            value="<?php echo esc_attr($details['topics']); ?>" 
+            type="text">
+   </label>
+               
+   <label class="te_label">
+      <span class="te_label_text">Duration</span>
+      <input    
+         id="te_course_ucas_code_field"
+         name="_te_course_array_fields[ucas_code]"
+         value="<?php echo esc_attr($details['ucas_code']);?>" 
+         type="text"  > 
+   </label>
+            
+   <label class="te_label">
+      <span class="te_label_text">Duration</span>
+      <input   
+         id="te_course_duration_field"
+         name="_te_course_array_fields[duration]" 
+         value="<?php echo esc_attr($details['duration']);?>"
+         type="text"  >
+   </label>
+
+   <label class="te_label">
+      <span class="te_label_text">Study Mode</span>
       <select name="_te_course_array_fields[study_mode]" id="te_course_study_mode_field">
-         <option value="Full Time"        <?php selected($details['study_mode'],'Full Time');?> >Full Time</option>
-         <option value="Part Time"        <?php selected($details['study_mode'],'Part Time');?> >Part Time</option>
-         <option value="Work Placement"   <?php selected($details['study_mode'],'Work Placement');?> >Work Placement</option>
+         <option value="Full Time" <?php selected($details['study_mode'],'Full Time');?> >Full Time</option>
+         <option value="Part Time" <?php selected($details['study_mode'],'Part Time');?> >Part Time</option>
+         <option value="Work Placement" <?php selected($details['study_mode'],'Work Placement');?> >Work Placement</option>
       </select>
-   </div>
-   <div class="form-field term-image-wrap">
-      <label for="te_course_custom_metabox_start_month">Start Month</label>
+   </label>
+
+   <label class="te_label">
+      <span class="te_label_text">Start Month</span>
       <select name="_te_course_array_fields[start_month]" id="te_course_start_month_field">
-         <option value="January"        <?php selected($details['start_month'],'January');?> >January</option>
-         <option value="February"        <?php selected($details['start_month'],'February');?> >February</option>
-         <option value="March"   <?php selected($details['start_month'],'March');?> >March</option>
-         <option value="April"   <?php selected($details['start_month'],'April');?> >April</option>
-         <option value="May"   <?php selected($details['start_month'],'May');?> >May</option>
-         <option value="June"   <?php selected($details['start_month'],'June');?> >June</option>
-         <option value="July"   <?php selected($details['start_month'],'July');?> >July</option>
-         <option value="August"   <?php selected($details['start_month'],'August');?> >August</option>
-         <option value="September"   <?php selected($details['start_month'],'September');?> >September</option>
-         <option value="October"   <?php selected($details['start_month'],'October');?> >October</option>
-         <option value="November"   <?php selected($details['start_month'],'November');?> >November</option>
-         <option value="December"   <?php selected($details['start_month'],'December');?> >December</option>
+         <option value="January" <?php selected($details['start_month'],'January');?> >January</option>
+         <option value="February" <?php selected($details['start_month'],'February');?> >February</option>
+         <option value="March" <?php selected($details['start_month'],'March');?> >March</option>
+         <option value="April" <?php selected($details['start_month'],'April');?> >April</option>
+         <option value="May" <?php selected($details['start_month'],'May');?> >May</option>
+         <option value="June" <?php selected($details['start_month'],'June');?> >June</option>
+         <option value="July" <?php selected($details['start_month'],'July');?> >July</option>
+         <option value="August" <?php selected($details['start_month'],'August');?> >August</option>
+         <option value="September" <?php selected($details['start_month'],'September');?> >September</option>
+         <option value="October" <?php selected($details['start_month'],'October');?> >October</option>
+         <option value="November" <?php selected($details['start_month'],'November');?> >November</option>
+         <option value="December" <?php selected($details['start_month'],'December');?> >December</option>
       </select>
-   </div>
-   <div class="form-field term-image-wrap">
-      <label for="te_course_custom_metabox_learning_mode">Learning Mode</label>
+   </label>
+
+   <label class="te_label">
+      <span class="te_label_text">Learning Mode</span>
       <select name="_te_course_array_fields[learning_mode]" id="te_course_learning_mode_field">
-         <option value="On Campus Learning"        <?php selected($details['learning_mode'],'On Campus Learning');?> >On Campus Learning</option>
-         <option value="Online Learning"        <?php selected($details['learning_mode'],'Online Learning');?> >Online Learning</option>
-      </select>               
-   </div>
-</div>
+         <option value="On Campus Learning" <?php selected($details['learning_mode'],'On Campus Learning');?> >On Campus Learning</option>
+         <option value="Online Learning" <?php selected($details['learning_mode'],'Online Learning');?> >Online Learning</option>
+      </select> 
+   </label>              
--- views/job_custom_post_metabox.php
+++ views/job_custom_post_metabox.php
@@ -5,74 +5,99 @@
 
 ?>
 
-<div class="form-wrap">
-   <div class="form-field term-image-wrap">
-      <label for="te_job_custom_metabox_school_or_section">School/Section</label>
-      <input value="<?php echo esc_attr($details['school_or_section']); ?>"
-               name="_te_job_array_fields[school_or_section]" id="te_school_or_section_field" type="text">
-   </div>
-   <div class="form-field term-image-wrap">
-      <label for="te_job_custom_metabox_staff_category">Staff Category</label>
-      <input value="<?php echo esc_attr($details['staff_category']);  ?>" type="text" 
-               name="_te_job_array_fields[staff_category]" id="te_job_staff_category_field" >
-   </div>
-   <div class="form-field term-image-wrap">
-      <label for="te_job_custom_metabox_position_type">Position Type</label>
-      <input value="<?php echo esc_attr($details['position_type']); ?>" type="text" 
-               name="_te_job_array_fields[position_type]" id="te_job_position_type_field" >
-   </div>
-   <div class="form-field term-image-wrap">
-      <label for="te_job_custom_metabox_duration">Duration of Post</label>
-      <input value="<?php echo esc_attr($details['duration']);  ?>" type="text" 
-               name="_te_job_array_fields[duration]" id="te_job_duration_field">
-   </div>
-   <div class="form-field term-image-wrap">
-      <label for="te_job_custom_metabox_grade">Grade</label>
+   <label class="te_label">
+      <span class="te_label_text">School/Section</span>
+      <input
+         id="te_school_or_section_field" 
+         name="_te_job_array_fields[school_or_section]" 
+         value="<?php echo esc_attr($details['school_or_section']); ?>"
+         type="text">      
+   </label>
+
+   <label class="te_label">
+      <span class="te_label_text">Staff Category</span>
+      <input
+         id="te_job_staff_category_field" 
+         name="_te_job_array_fields[staff_category]" 
+         value="<?php echo esc_attr($details['staff_category']);  ?>" 
+         type="text" >         
+   </label>
+
+   <label class="te_label">
+      <span class="te_label_text">Position Type</span>
+      <input
+         id="te_job_position_type_field" 
+         name="_te_job_array_fields[position_type]" 
+         value="<?php echo esc_attr($details['position_type']); ?>" 
+         type="text">
+   </label>
+
+   <label class="te_label">
+      <span class="te_label_text">Duration of Post</span>
+      <input 
+         id="te_job_duration_field"
+         name="_te_job_array_fields[duration]"
+         value="<?php echo esc_attr($details['duration']);  ?>" 
+         type="text" >
+   </label>
+
+   <label class="te_label">
+      <span class="te_label_text">Grade</span>         
       <select name="_te_job_array_fields[grade]" id="te_job_grade_field">
-         <option value="n/a"        <?php selected($details['grade'],'n/a');?> >n/a</option>
-         <option value="Grade 1"   <?php selected($details['grade'],'Grade 1');?> >Grade 1</option>
-         <option value="Grade 2"   <?php selected($details['grade'],'Grade 2');?> >Grade 2</option>
-         <option value="Grade 3"   <?php selected($details['grade'],'Grade 3');?> >Grade 3</option>
-         <option value="Grade 4"   <?php selected($details['grade'],'Grade 4');?> >Grade 4</option>
-         <option value="Grade 5"   <?php selected($details['grade'],'Grade 5');?> >Grade 5</option>
-         <option value="Grade 6"   <?php selected($details['grade'],'Grade 6');?> >Grade 6</option>
-         <option value="Grade 7"   <?php selected($details['grade'],'Grade 7');?> >Grade 7</option>
-         <option value="Grade 8"   <?php selected($details['grade'],'Grade 8');?> >Grade 8</option>
-         <option value="Grade 9"   <?php selected($details['grade'],'Grade 9');?> >Grade 9</option>
-         <option value="Grade 10"  <?php selected($details['grade'],'Grade 10');?> >Grade 10</option>
+         <option value="n/a" <?php selected($details['grade'],'n/a');?> >n/a</option>
+         <option value="Grade 1" <?php selected($details['grade'],'Grade 1');?> >Grade 1</option>
+         <option value="Grade 2" <?php selected($details['grade'],'Grade 2');?> >Grade 2</option>
+         <option value="Grade 3" <?php selected($details['grade'],'Grade 3');?> >Grade 3</option>
+         <option value="Grade 4" <?php selected($details['grade'],'Grade 4');?> >Grade 4</option>
+         <option value="Grade 5" <?php selected($details['grade'],'Grade 5');?> >Grade 5</option>
+         <option value="Grade 6" <?php selected($details['grade'],'Grade 6');?> >Grade 6</option>
+         <option value="Grade 7" <?php selected($details['grade'],'Grade 7');?> >Grade 7</option>
+         <option value="Grade 8" <?php selected($details['grade'],'Grade 8');?> >Grade 8</option>
+         <option value="Grade 9" <?php selected($details['grade'],'Grade 9');?> >Grade 9</option>
+         <option value="Grade 10"<?php selected($details['grade'],'Grade 10');?> >Grade 10</option>
       </select>
-   </div>
-   <div class="form-field term-image-wrap">
-      <label for="te_job_custom_metabox_salary">Salary</label>
+   </label>
+ 
+
+   <label class="te_label">
+      <span class="te_label_text">Salary</span>
       <select name="_te_job_array_fields[salary]" id="te_job_salary_field">
-         <option value="£19,000 - £20,000"   <?php selected($details['salary'],'£19,000 - £20,000');?> >£19,000 - £20,000</option>
-         <option value="£20,000 - £21,000"   <?php selected($details['salary'],'£20,000 - £21,000');?> >£20,000 - £21,000</option>
-         <option value="£21,000 - £22,000"   <?php selected($details['salary'],'£21,000 - £22,000');?> >£21,000 - £22,000</option>
-         <option value="£22,000 - £23,000"   <?php selected($details['salary'],'£22,000 - £23,000');?> >£22,000 - £23,000</option>
-         <option value="£23,000 - £24,000"   <?php selected($details['salary'],'£23,000 - £24,000');?> >£23,000 - £24,000</option>
-         <option value="£24,000 - £25,000"   <?php selected($details['salary'],'£24,000 - £25,000');?> >£24,000 - £25,000</option>
-         <option value="£25,000 - £26,000"   <?php selected($details['salary'],'£25,000 - £26,000');?> >£25,000 - £26,000</option>
-         <option value="£26,000 - £27,000"   <?php selected($details['salary'],'£26,000 - £27,000');?> >£26,000 - £27,000</option>
-         <option value="£27,000 - £28,000"   <?php selected($details['salary'],'£27,000 - £28,000');?> >£27,000 - £28,000</option>
-         <option value="£28,000 - £29,000"   <?php selected($details['salary'],'£28,000 - £29,000');?> >£28,000 - £29,000</option>
+         <option value="£19,000 - £20,000" <?php selected($details['salary'],'£19,000 - £20,000');?> >£19,000 - £20,000</option>
+         <option value="£20,000 - £21,000" <?php selected($details['salary'],'£20,000 - £21,000');?> >£20,000 - £21,000</option>
+         <option value="£21,000 - £22,000" <?php selected($details['salary'],'£21,000 - £22,000');?> >£21,000 - £22,000</option>
+         <option value="£22,000 - £23,000" <?php selected($details['salary'],'£22,000 - £23,000');?> >£22,000 - £23,000</option>
+         <option value="£23,000 - £24,000" <?php selected($details['salary'],'£23,000 - £24,000');?> >£23,000 - £24,000</option>
+         <option value="£24,000 - £25,000" <?php selected($details['salary'],'£24,000 - £25,000');?> >£24,000 - £25,000</option>
+         <option value="£25,000 - £26,000" <?php selected($details['salary'],'£25,000 - £26,000');?> >£25,000 - £26,000</option>
+         <option value="£26,000 - £27,000" <?php selected($details['salary'],'£26,000 - £27,000');?> >£26,000 - £27,000</option>
+         <option value="£27,000 - £28,000" <?php selected($details['salary'],'£27,000 - £28,000');?> >£27,000 - £28,000</option>
+         <option value="£28,000 - £29,000" <?php selected($details['salary'],'£28,000 - £29,000');?> >£28,000 - £29,000</option>
       </select>
-   </div>
-   <div class="form-field term-image-wrap">
-      <label for="te_job_custom_metabox_location">Location</label>
+   </label>
+      
+   <label class="te_label">
+      <span class="te_label_text">Location</span>         
       <select name="_te_job_array_fields[location]" id="te_job_location_field">
-         <option value="Aberdeen"   <?php selected($details['location'],'Aberdeen');?> >Aberdeen</option>
-         <option value="Culterty"   <?php selected($details['location'],'Culterty');?> >Culterty</option>
-         <option value="Banchory"   <?php selected($details['location'],'Banchory');?> >Banchory</option>
+         <option value="Aberdeen" <?php selected($details['location'],'Aberdeen');?> >Aberdeen</option>
+         <option value="Culterty" <?php selected($details['location'],'Culterty');?> >Culterty</option>
+         <option value="Banchory" <?php selected($details['location'],'Banchory');?> >Banchory</option>
       </select>
-   </div>
-   <div class="form-field term-image-wrap">
-      <label for="te_job_custom_metabox_ref">Ref.</label>
-      <input value="<?php echo esc_attr($details['ref']);  ?>" type="text" 
-               name="_te_job_array_fields[ref]" id="te_job_ref_field">
-   </div>
-   <div class="form-field term-image-wrap">
-      <label for="te_job_custom_metabox_closing_date">Closing Date</label>
-      <input value="<?php echo esc_attr($details['closing_date']);  ?>" type="text" 
-               name="_te_job_array_fields[closing_date]" id="te_job_closing_date_field">
-   </div>
-</div>
+   </label>
+      
+   <label class="te_label">
+      <span class="te_label_text">Ref.</span>      
+      <input 
+         id="te_job_ref_field" 
+         name="_te_job_array_fields[ref]" 
+         value="<?php echo esc_attr($details['ref']);  ?>"
+         type="text">
+   </label>
+      
+   <label class="te_label">
+      <span class="te_label_text">Closing Date</span>
+      <input
+         id="te_job_closing_date_field" 
+         name="_te_job_array_fields[closing_date]" 
+         value="<?php echo esc_attr($details['closing_date']);  ?>"
+         type="text">         
+   </label>
